45026150ac8ade07369d94d897aaaa9efdd85722
Giel van der Weerd <gvdweerd@me.com>
2023-08-14 12:22:08 +0200

    Analyse manoeuvring space of a set of SPORES; cluster spores to scenarios using KMeans clustering and elbow plot, visualise power capacity for each scenario in a stacked barchart and add distribution plot of paper metrics of each scenario

---
+++ src/pathways.py
@@ -0,0 +1,9 @@
+import os
+import pandas
+
+from src.utils.parameters import *
+from src.utils.visualisation import *
+from src.utils.data_io import *
+
+if __name__ == '__main__':
+    print("")
--- src/utils/visualisation.py
+++ src/utils/visualisation.py
@@ -40,6 +40,62 @@ POWER_TECH_COLORS = {
     "Coal": "#000000", # Black = #000000, Grey = #808080
     "coal": "#000000"
 }
+metric_plot_order = {
+    "curtailment": 2,
+    "electricity_production_gini": 5,
+    "average_national_import": 4,
+    "fuel_autarky_gini": 6,
+    "storage_discharge_capacity": 1,
+    "transport_electrification": 9,
+    "heat_electrification": 8,
+    "biofuel_utilisation": 3,
+    "ev_flexibility": 7
+}
+metric_plot_names = {
+    "curtailment": 'Curtailment',
+    "electricity_production_gini":  'Electricity production\nGini coefficient',
+    "average_national_import": 'Average\nnational import',
+    "fuel_autarky_gini": 'Fuel autarky\nGini coefficient',
+    "storage_discharge_capacity": 'Storage discharge\ncapacity',
+    "transport_electrification": 'Transport\nelectrification',
+    "heat_electrification": 'Heat\nelectrification',
+    "biofuel_utilisation": 'Biofuel\nutilisation',
+    "ev_flexibility": 'EV as flexibility'
+}
+metric_range_formatting = {
+    "curtailment": lambda x: x.round(0).astype(int),
+    "electricity_production_gini": lambda x: x.round(2),
+    "average_national_import": lambda x: x.round(0).astype(int),
+    "fuel_autarky_gini": lambda x: x.round(2),
+    "storage_discharge_capacity": lambda x: x.round(0).astype(int) if x.name == "max" else x.round(2),
+    "transport_electrification": lambda x: x.round(0).astype(int),
+    "heat_electrification": lambda x: x.round(0).astype(int),
+    "biofuel_utilisation": lambda x: x.round(0).astype(int),
+    "ev_flexibility": lambda x: x.round(2)
+}
+FIGWIDTH = 6.77165
+
+
+def get_color_dict2(label_list):
+    color_palette = sns.color_palette("bright")
+    colors = {
+        label_list[i]: color_palette[i % len(color_palette)]
+        for i in range(len(label_list))
+    }
+    colors["rest_color"] = color_palette[-3]
+    return colors
+
+
+
+def get_color_dict(label_list):
+    # Define colours
+    colors = {
+        label_list[i]: (sns.color_palette("bright")[:-3] + sns.color_palette("bright")[-2:])[i]
+        for i in range(len(label_list))
+    }
+    colors["rest_color"] = sns.color_palette("bright")[-3]
+    return colors
+
 
 if __name__ == '__main__':
     print("")
